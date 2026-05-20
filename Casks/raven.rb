fe21d636603bc2dfbe0f36ef9f66e58dca72b459
Arthur Laisney <77444753+ArthurLsy@users.noreply.github.com>
2026-05-20 16:45:34 +0200

    add Raven

---
+++ Casks/raven.rb
@@ -0,0 +1,11 @@
+cask "raven" do
+  version "1.0.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  url "https://github.com/ArthurLsy/raven/releases/download/0.0.1/Raven-macos-silicon.tar.gz"
+  name "Raven"
+  desc "Git manager"
+  homepage "https://github.com/ArthurLsy/raven"
+
+  app "Raven.app"
+end
