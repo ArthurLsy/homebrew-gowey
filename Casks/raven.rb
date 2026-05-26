e41e4b4287f135aec281feefa089d37442c3bb4e
Arthur Laisney <alaisney7@gmail.com>
2026-05-26 09:59:56 +0200

    fix

---
+++ Casks/raven.rb
@@ -0,0 +1,12 @@
+  cask "raven" do
+    version "0.0.1"
+    sha256 "1cfdec74654f9c48a92cac43e468165551f0ded074c11fbe4af98daa057f0ffe"
+
+    url "https://github.com/arthurlsy/raven/releases/download/#{version}/Raven-macos-silicon.tar.gz"
+    name "Raven"
+    desc "Cross-platform Git desktop client"
+    homepage "https://github.com/arthurlsy/raven"
+
+    app "Raven.app"
+  end
+
